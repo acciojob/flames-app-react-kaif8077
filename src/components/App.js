3594078041bfe97cae751f9fb21d59ea2f685d87
kaif8077 <149231751+kaif8077@users.noreply.github.com>
2025-11-30 12:01:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,99 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
+function App() {
+    const [name1, setName1] = useState("");
+    const [name2, setName2] = useState("");
+    const [result, setResult] = useState("");
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    const calculateFLAMES = () => {
+
+        if (name1.trim() === "" || name2.trim() === "") {
+            setResult("Please Enter valid input");
+            return;
+        }
+
+        let arr1 = name1.split("");
+        let arr2 = name2.split("");
+
+        
+        for (let i = 0; i < arr1.length; i++) {
+            const char = arr1[i];
+            const indexInArr2 = arr2.indexOf(char);
+
+            if (indexInArr2 !== -1) {
+                arr1[i] = "";         
+                arr2[indexInArr2] = "";  
+            }
+        }
+
+       
+        const count = arr1.join("").length + arr2.join("").length;
+
+        
+        const mod = count % 6;
+
+        const map = [
+            "Siblings",
+            "Friends",
+            "Lovers",
+            "Affection",
+            "Marriage",
+            "Enemies"
+        ];
+
+        setResult(map[mod]);
+    };
+
+    const clearAll = () => {
+        setName1("");
+        setName2("");
+        setResult("");
+    };
+
+    return (
+        <div id="main">
+            <form>
+                <input
+                    data-testid="input1"
+                    name="name1"
+                    type="text"
+                    value={name1}
+                    onChange={(e) => setName1(e.target.value)}
+                    placeholder="Enter Name 1"
+                />
+
+                <input
+                    data-testid="input2"
+                    name="name2"
+                    type="text"
+                    value={name2}
+                    onChange={(e) => setName2(e.target.value)}
+                    placeholder="Enter Name 2"
+                />
+
+                <button
+                    data-testid="calculate_relationship"
+                    name="calculate_relationship"
+                    type="button"
+                    onClick={calculateFLAMES}
+                >
+                    Calculate Relationship Future
+                </button>
+
+                <button
+                    data-testid="clear"
+                    name="clear"
+                    type="button"
+                    onClick={clearAll}
+                >
+                    Clear
+                </button>
+            </form>
+
+            <h3 data-testid="answer">{result}</h3>
+        </div>
+    );
 }
-
 
 export default App;
